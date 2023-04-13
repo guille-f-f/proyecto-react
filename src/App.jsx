18b50be166485c29cd9a75a5b1a3deb319d36bbd
Guillermo F Funaro <funaro.g.f@gmail.com>
2023-04-13 09:02:42 -0300

    carga inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { createContext, useContext } from 'react'
+import BaseDeDatos from './datos/productos.json'
+import UltimasOfertas from './componentes/UltimasOfertas'
+
+export const Productos = createContext(); 
 
 function App() {
   return (
     <div className="App">
-      <h1>Bienvenidos a nuestro proyecto en React!!</h1>
+      <Productos.Provider value={BaseDeDatos}>
+        <UltimasOfertas />
+      </Productos.Provider>
     </div>
   )
 }
---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../estilos/Boton.css';
+
+const Boton = () => {
+  return (
+    <button className='boton'>SHOWROOM</button>
+  )
+}
+
+export default Boton
