0edf6b15d394451980d2a5d75c331ffb2a610f18
Nicole Didier <didiernickygmail.com>
2023-04-18 19:07:18 -0300

    commit-nueva-coleccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import './Componentes/BarraNavegacion/Navbar'
 import Navbar from './Componentes/BarraNavegacion/Navbar'
+import Coleccion from './Componentes/NuevaColeccion/Coleccion'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Coleccion/>
     </div>
   )
 }
