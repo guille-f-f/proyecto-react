8dd2dec8ffb5875e10751c43df92c5e181eef816
Nicole Didier <didiernickygmail.com>
2023-04-21 11:57:09 -0300

    commite-carrusel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import './Componentes/BarraNavegacion/Navbar'
 import Navbar from './Componentes/BarraNavegacion/Navbar'
 import Coleccion from './Componentes/NuevaColeccion/Coleccion'
+import Carrusel from './Componentes/Carrusel/Carrusel'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Coleccion/>
+      <Carrusel/>
+      
     </div>
   )
 }
