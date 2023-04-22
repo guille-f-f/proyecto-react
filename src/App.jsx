362b40b8be7b3cf4225bcdd90c23a7512294ae21
Nicole Didier <didiernickygmail.com>
2023-04-21 21:06:31 -0300

    agregando componentes en homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import './Componentes/BarraNavegacion/Navbar'
-import Navbar from './Componentes/BarraNavegacion/Navbar'
-import Coleccion from './Componentes/NuevaColeccion/Coleccion'
-import Carrusel from './Componentes/Carrusel/Carrusel'
 import './estilos/App.css'
 import { createContext } from 'react'
 import BaseDeDatos from './datos/productos.json'
@@ -12,8 +8,6 @@ export const Productos = createContext();
 function App() {
   return (
     <div className="App">
-     
-      
       <Productos.Provider value={BaseDeDatos}>
         <Rutas />
       </Productos.Provider>
