8bf82605dcc1d17dac2e30f31fda1136e6fe4c68
Nicole Didier <didiernickygmail.com>
2023-04-26 13:11:09 -0300

    tarjetas-confort

--- src/paginas/Homepage.jsx
+++ src/paginas/Homepage.jsx
@@ -3,12 +3,14 @@ import UltimasOfertas from "../componentes/UltimasOfertas";
 import Carrusel from "../Componentes/Carrusel/Carrusel";
 import Coleccion from "../Componentes/NuevaColeccion/Coleccion";
 import Portada from "../Componentes/Portada";
+import TarjetasConfort from "../Componentes/TarjetasConfort";
 
 const Homepage = () => {
   return (
   <>
   <Portada />
   <UltimasOfertas />
+  <TarjetasConfort />
   <Coleccion />
   <Carrusel />
   </>
