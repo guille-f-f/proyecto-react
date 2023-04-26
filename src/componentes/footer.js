ab39998873c8673942d5f561b87a711271b91743
mekamor <martinraisberg@gmail.com>
2023-04-26 14:15:19 -0300

    listo

---
+++ src/componentes/footer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../estilos/footer.css';
+
+const footer = () =>{
+    return (
+        <footer>
+    <div class="pie-de-pagina1">
+        <div class="box">
+            <a href="#" target="_blank" class="fab fa-facebook"></a>
+            <a href="#" target="_blank" class="fab fa-instagram"></a>
+            <a href="#" target="_blank" class="fab fa-twitter"></a>
+            <a href="#" target="_blank" class="fab fa-whatsapp"></a>
+            <a href="#" target="_blank" class="fab fa-facebook-messenger"></a>
+            <a href="#" target="_blank" class="fab fa-telegram"></a>
+        </div>
+    </div>
+    <div class="pie-de-pagina2">
+    <div class="box2">
+        <h1>COMPARTE CON TUS AMIGOS</h1>
+    </div>
+</div>
+    <div class="pie-de-pagina3">
+    <div class="box">
+        <figure>
+            <a href="#">
+            </a>
+        </figure>
+    </div>
+    <div class="box">
+        <h2>Accesorios</h2>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+    </div>
+    <div class="box">
+        <h2>Categorias</h2>
+        <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+            </div>
+            <div class="box">
+                <h2>Promos</h2>
+            </div>
+            <div class="box">
+                <h2>Acerca de nosotros</h2>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default footer 
