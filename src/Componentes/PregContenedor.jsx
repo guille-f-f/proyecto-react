b1bc20ea6c14f3b3ab02006df31db69a836cce75
Nicole Didier <didiernickygmail.com>
2023-05-04 11:14:28 -0300

    preguntas-json

---
+++ src/Componentes/PregContenedor.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../estilos/Preguntas.css'
+
+const PregContenedor = (props) => {
+  return (
+    <section >
+        <h1 className='preguntas_titulo'>Preguntas Frecuentes</h1>
+        <div >
+            {props.children}
+        </div>
+    </section>
+  )
+}
+
+export default PregContenedor
--- src/paginas/Homepage.jsx
+++ src/paginas/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import UltimasOfertas from "../componentes/UltimasOfertas";
-import Carrusel from "../Componentes/Carrusel/Carrusel";
-import Coleccion from "../Componentes/NuevaColeccion/Coleccion";
+import Carrusel from "../Componentes/Carrusel";
+import Coleccion from "../Componentes/Coleccion";
 import Portada from "../Componentes/Portada";
 import TarjetasConfort from "../Componentes/TarjetasConfort";
 import Descuento from "../Componentes/Descuento";
