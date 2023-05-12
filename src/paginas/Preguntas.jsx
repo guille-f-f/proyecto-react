baeffe5ef56571e58b008202d11453064b71f50a
Guillermo F Funaro <funaro.g.f@gmail.com>
2023-05-11 22:52:37 -0300

    commit cierre 4

--- src/paginas/Preguntas.jsx
+++ src/paginas/Preguntas.jsx
@@ -1,6 +1,6 @@
 import TarjetaPreguntas from "../componentes/TarjetaPreguntas";
 import PregContenedor from "../componentes/PregContenedor";
-import datos from "../datos/preguntas.json";
+import datos from "../datos/reguntas.json";
 
 const Preguntas = () => {
   return (
